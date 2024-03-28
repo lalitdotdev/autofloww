6bbd581a4417990cbc418b256ec9aaf273f4d481
Lalit sharma <geekymonkcp742k@gmail.com>
2024-03-28 18:42:04 +0530

    Added Toaster component for displaying notifications in RootLayout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { DM_Sans } from "next/font/google";
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -17,6 +18,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
+
+
         <html lang="en">
             <body className={font.className}>
                 <ThemeProvider
@@ -26,8 +29,10 @@ export default function RootLayout({
                     disableTransitionOnChange
                 >
                     {children}
+                    <Toaster />
                 </ThemeProvider>
             </body>
         </html>
+
     );
 }
