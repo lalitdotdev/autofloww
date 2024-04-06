98fde020577614e8ddc55eb10e04ba6ff28e669a
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-06 16:12:47 +0530

    feat(connection constants): Add connection data for Google Drive, Discord, Notion, and Slack .

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,5 @@
 import Category from '@/components/icons/category';
+import { Connection } from './types';
 import Home from '@/components/icons/home';
 import Logs from '@/components/icons/clipboard';
 import Payment from '@/components/icons/payment';
@@ -143,35 +144,34 @@ export const EditorCanvasDefaultCardTypes = {
   },
 };
 
-// export const CONNECTIONS: Connection[] = [
-//   {
-//     title: 'Google Drive',
-//     description: 'Connect your google drive to listen to folder changes',
-//     image: '/googleDrive.png',
-//     connectionKey: 'googleNode',
-//     alwaysTrue: true,
-//   },
-//   {
-//     title: 'Discord',
-//     description: 'Connect your discord to send notification and messages',
-//     image: '/discord.png',
-//     connectionKey: 'discordNode',
-//     accessTokenKey: 'webhookURL',
-//   },
-//   {
-//     title: 'Notion',
-//     description: 'Create entries in your notion dashboard and automate tasks.',
-//     image: '/notion.png',
-//     connectionKey: 'notionNode',
-//     accessTokenKey: 'accessToken',
-//   },
-//   {
-//     title: 'Slack',
-//     description: 'Use slack to send notifications to team members through your own custom bot.',
-//     image: '/slack.png',
-//     connectionKey: 'slackNode',
-//     accessTokenKey: 'slackAccessToken',
-//     slackSpecial: true,
-//   },
-// ];
-/*  */
+export const CONNECTIONS: Connection[] = [
+  {
+    title: 'Google Drive',
+    description: 'Connect your google drive to listen to folder changes',
+    image: '/googleDrive.png',
+    connectionKey: 'googleNode',
+    alwaysTrue: true,
+  },
+  {
+    title: 'Discord',
+    description: 'Connect your discord to send notification and messages',
+    image: '/discord.png',
+    connectionKey: 'discordNode',
+    accessTokenKey: 'webhookURL',
+  },
+  {
+    title: 'Notion',
+    description: 'Create entries in your notion dashboard and automate tasks.',
+    image: '/notion.png',
+    connectionKey: 'notionNode',
+    accessTokenKey: 'accessToken',
+  },
+  {
+    title: 'Slack',
+    description: 'Use slack to send notifications to team members through your own custom bot.',
+    image: '/slack.png',
+    connectionKey: 'slackNode',
+    accessTokenKey: 'slackAccessToken',
+    slackSpecial: true,
+  },
+];
