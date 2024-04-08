3d4a65b051577f2856af250ff376e41a7d5edf5a
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-08 17:42:12 +0530

    feat(editor-utility handlers): Implement onSlackContent handler to update Slack connection content within nodeConnection based on input changes.

--- src/lib/editor-utils.ts
+++ src/lib/editor-utils.ts
@@ -1,3 +1,4 @@
+import { ConnectionProviderProps } from '@/providers/connections-provider';
 import { EditorCanvasCardType } from './types';
 
 // Setting the data in the event dataTransfer object to the nodeType and setting the effectAllowed to move the node around the canvas so that it can be dragged and dropped to the canvas from the sidebar area of the editor canvas sidebar component.
@@ -5,3 +6,11 @@ export const onDragStart = (event: any, nodeType: EditorCanvasCardType['type']) 
   event.dataTransfer.setData('application/reactflow', nodeType);
   event.dataTransfer.effectAllowed = 'move';
 };
+
+export const onSlackContent = (nodeConnection: ConnectionProviderProps, event: React.ChangeEvent<HTMLInputElement>) => {
+  nodeConnection.setSlackNode((prev: any) => ({
+    ...prev,
+    content: event.target.value,
+  }));
+};
+
