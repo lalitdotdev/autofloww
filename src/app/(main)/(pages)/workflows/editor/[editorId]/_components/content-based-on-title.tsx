b2078cb345cc2523d0a428e88cf5d9ee96396168
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-08 18:55:35 +0530

    feat(Content based on title ): Define ContentBasedOnTitle for dynamic connection handling (nodeMapper, access token checks) & address potential type safety issues.

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/content-based-on-title.tsx
@@ -0,0 +1,83 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import React, { useEffect } from 'react'
+
+import { AccordionContent } from '@/components/ui/accordion'
+import ActionButton from './action-button'
+import { ConnectionProviderProps } from '@/providers/connections-provider'
+import { EditorState } from '@/providers/editor-provider'
+import GoogleDriveFiles from './google-drive-files'
+import GoogleFileDetails from './google-file-details'
+import { Input } from '@/components/ui/input'
+import { nodeMapper } from '@/lib/types'
+import { onContentChange } from '@/lib/editor-utils'
+import { toast } from 'sonner'
+
+export interface Option {
+    value: string
+    label: string
+    disable?: boolean
+    /** fixed option that can't be removed. */
+    fixed?: boolean
+    /** Group the options by providing key. */
+    [key: string]: string | boolean | undefined
+}
+interface GroupOption {
+    [key: string]: Option[]
+}
+
+type Props = {
+    nodeConnection: ConnectionProviderProps
+    newState: EditorState
+    file: any
+    setFile: (file: any) => void
+    selectedSlackChannels: Option[]
+    setSelectedSlackChannels: (value: Option[]) => void
+}
+
+const ContentBasedOnTitle = ({
+    nodeConnection,
+    newState,
+    file,
+    setFile,
+    selectedSlackChannels,
+    setSelectedSlackChannels,
+}: Props) => {
+    const { selectedNode } = newState.editor
+    const title = selectedNode.data.title
+
+
+
+    // @ts-ignore
+    const nodeConnectionType: any = nodeConnection[nodeMapper[title]]
+    if (!nodeConnectionType) return <p>Not connected</p>
+
+    const isConnected =
+        title === 'Google Drive'
+            ? !nodeConnection.isLoading
+            : !!nodeConnectionType[
+            `${title === 'Slack'
+                ? 'slackAccessToken'
+                : title === 'Discord'
+                    ? 'webhookURL'
+                    : title === 'Notion'
+                        ? 'accessToken'
+                        : ''
+            }`
+            ]
+
+    if (!isConnected) return <p>Not connected</p>
+
+    return (
+        <div>
+            Content based on title.
+        </div>
+    )
+}
+
+export default ContentBasedOnTitle
