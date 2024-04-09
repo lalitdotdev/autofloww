eeb23b6c7c11161b45427ae06bcc9f2a2b1d7fd0
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-09 13:43:29 +0530

    data(Connections-Workflows / Server Actions ) 🚦 : Implement onCreateNodesEdges to update workflow nodes, edges, and path in database.

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_actions/workflow-connections.tsx
@@ -0,0 +1,24 @@
+'use server'
+
+import { db } from '@/lib/db'
+
+export const onCreateNodesEdges = async (
+    flowId: string,
+    nodes: string,
+    edges: string,
+    flowPath: string
+) => {
+    const flow = await db.workflows.update({
+        where: {
+            id: flowId,
+        },
+        data: {
+            nodes,
+            edges,
+            flowPath: flowPath,
+        },
+    })
+
+    if (flow) return { message: 'flow saved' }
+}
+
