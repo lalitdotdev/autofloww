d88baee8b173efc690eb48d53e15f347c3de5eae
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-09 13:45:11 +0530

    data(onFlowPublish / Server Actions ) 🚦 : Implement onFlowPublish to update workflow publish state in database.

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_actions/workflow-connections.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_actions/workflow-connections.tsx
@@ -22,3 +22,17 @@ export const onCreateNodesEdges = async (
     if (flow) return { message: 'flow saved' }
 }
 
+export const onFlowPublish = async (workflowId: string, state: boolean) => {
+    console.log(state)
+    const published = await db.workflows.update({
+        where: {
+            id: workflowId,
+        },
+        data: {
+            publish: state,
+        },
+    })
+
+    if (published.publish) return 'Workflow published'
+    return 'Workflow unpublished'
+}
