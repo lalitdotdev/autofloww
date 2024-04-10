ba2a3161635a907bffd742e2549f3dc629fc3987
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-10 11:40:06 +0530

    data(Slack / listBotChannels), ui(Editor): Implement listBotChannels to retrieve available Slack channels (public & private, max 200) for the bot using access token.

--- src/app/(main)/(pages)/connections/_actions/slack-connection.tsx
+++ src/app/(main)/(pages)/connections/_actions/slack-connection.tsx
@@ -1,5 +1,7 @@
 'use server'
 
+import { Option } from '@/components/ui/multiple-selector'
+import axios from 'axios'
 import { currentUser } from '@clerk/nextjs'
 import { db } from '@/lib/db'
 
@@ -48,3 +50,32 @@ export const getSlackConnection = async () => {
     return null
 }
 
+export async function listBotChannels(
+    slackAccessToken: string
+): Promise<Option[]> {
+    const url = `https://slack.com/api/conversations.list?${new URLSearchParams({
+        types: 'public_channel,private_channel',
+        limit: '200',
+    })}`
+
+    try {
+        const { data } = await axios.get(url, {
+            headers: { Authorization: `Bearer ${slackAccessToken}` },
+        })
+
+        console.log(data)
+
+        if (!data.ok) throw new Error(data.error)
+
+        if (!data?.channels?.length) return []
+
+        return data.channels
+            .filter((ch: any) => ch.is_member)
+            .map((ch: any) => {
+                return { label: ch.name, value: ch.id }
+            })
+    } catch (error: any) {
+        console.error('Error listing bot channels:', error.message)
+        throw error
+    }
+}
