f0465a7fd70e401dcee51d17b1fb49dcc932b3c6
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-12 09:24:08 +0530

    feat(All Workflows Component): Implement Workflows component to display user workflows. Fetches workflows using onGetWorkflows, renders Workflow component for each workflow, and displays 'No Workflows' message if none found.

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import Workflow from './workflow'
+import { onGetWorkflows } from '../_actions/workflow-connections';
 
 type Props = {}
 
 const Workflows = async (props: Props) => {
+    const workflows = await onGetWorkflows();
 
     return (
         <div className="relative flex flex-col gap-4">
             <section className="flex flex-col m-2">
-                <Workflow
-                    name="Test"
-                    description="This is a test workflow"
-                    id="123"
-                    publish={true}
-
-                />
+                {workflows?.length ? (
+                    workflows.map((flow) => (
+                        <Workflow
+                            key={flow.id}
+                            {...flow}
+                        />
+                    ))
+                ) : (
+                    <div className="mt-28 flex text-muted-foreground items-center justify-center">
+                        No Workflows
+                    </div>
+                )}
             </section>
         </div>
     )
