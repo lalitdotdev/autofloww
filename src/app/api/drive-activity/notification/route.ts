84f5c6f9e50a4ed4dca2474f8053f4708e73e0d1
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-13 13:06:24 +0530

    feat(workflows-discord / activity-notifications ): Implement notification logic for 'Discord' step. Iterates over workflows, parses flow path, checks for 'Discord' step, retrieves webhook URL, and sends notification if available (removing processed step).

--- src/app/api/drive-activity/notification/route.ts
+++ src/app/api/drive-activity/notification/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest } from 'next/server';
 import { db } from '@/lib/db';
 import { headers } from 'next/headers';
+import { postContentToWebHook } from '@/app/(main)/(pages)/connections/_actions/discord-connection';
 
 export async function POST(req: NextRequest) {
   console.log('🔴 Changed');
@@ -26,15 +27,37 @@ export async function POST(req: NextRequest) {
           userId: user.clerkId,
         },
       });
+      if (workflow) {
+        workflow.map(async (flow) => {
+          const flowPath = JSON.parse(flow.flowPath!);
+          let current = 0;
+          while (current < flowPath.length) {
+            if (flowPath[current] == 'Discord') {
+              const discordMessage = await db.discordWebhook.findFirst({
+                where: {
+                  userId: flow.userId,
+                },
+                select: {
+                  url: true,
+                },
+              });
+              if (discordMessage) {
+                await postContentToWebHook(flow.discordTemplate!, discordMessage.url);
+                flowPath.splice(flowPath[current], 1);
+              }
+            }
+          }
+        });
+      }
     }
-  }
 
-  return Response.json(
-    {
-      message: 'success',
-    },
-    {
-      status: 200,
-    },
-  );
+    return Response.json(
+      {
+        message: 'success',
+      },
+      {
+        status: 200,
+      },
+    );
+  }
 }
