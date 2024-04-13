e98f19d169585a14bf5a0a4dc35b8c876e8e9303
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-13 10:21:33 +0530

    logic(googleapi / drive-activity notifications): Retrieve user information based on received Google Drive change notification resource ID. Looks up user in database using 'googleResourceId' and selects clerkId and credits.

--- src/app/api/drive-activity/notification/route.ts
+++ src/app/api/drive-activity/notification/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest } from 'next/server';
 import { headers } from 'next/headers';
+import { db } from '@/lib/db';
 
 export async function POST(req: NextRequest) {
+  console.log('🔴 Changed');
   const headerlist = headers();
   let channelResourceId;
   headerlist.forEach((value, key) => {
@@ -9,6 +11,15 @@ export async function POST(req: NextRequest) {
       channelResourceId = value;
     }
   });
+
+
+  if (channelResourceId) {
+    const user = await db.user.findFirst({
+      where: {
+        googleResourceId: channelResourceId,
+      },
+      select: { clerkId: true, credits: true },
+    })
 
   return Response.json(
     {
