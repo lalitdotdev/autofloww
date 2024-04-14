88f38a1be65d5fc17836dbc96996c2c7571ddd25
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-14 11:21:11 +0530

    feat(editor-canvas): Implement client-side ReactFlow canvas for workflow editing. Renders dynamic nodes/edges from editor state, memoizes 'onDragOver' for performance.

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import 'reactflow/dist/style.css'
+
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import ReactFlow, {
+    Background,
+    Controls,
+    MiniMap,
+    ReactFlowInstance,
+} from 'reactflow'
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from '@/components/ui/resizable'
+
+import EditorCanvasSidebar from './editor-canvas-sidebar'
+import { EditorNodeType } from '@/lib/types'
+import FlowInstance from './flow-instance'
+import { useEditor } from '@/providers/editor-provider'
+
+type Props = {}
+
+const initialNodes: EditorNodeType[] = []
+
+const initialEdges: { id: string; source: string; target: string }[] = []
+
+const EditorCanvas = (props: Props) => {
+    const { dispatch, state } = useEditor()
+    const [nodes, setNodes] = useState(initialNodes)
+    const [edges, setEdges] = useState(initialEdges)
+    const [isWorkFlowLoading, setIsWorkFlowLoading] = useState<boolean>(false)
+    const [reactFlowInstance, setReactFlowInstance] =
+        useState<ReactFlowInstance>()
+
+
+    const onDragOver = useCallback((event: any) => {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = 'move'
+    }, [])
+
+
+
+    return (
+        <ResizablePanelGroup direction="horizontal">
+            <ResizablePanel defaultSize={70}>
+                <div className="flex h-full items-center justify-center">
+                    <div
+                        style={{ width: '100%', height: '100%', paddingBottom: '70px' }}
+                        className="relative"
+                    >
+
+
+                        <ReactFlow
+                            className="w-[300px]"
+                            // onDrop={onDrop}
+                            onDragOver={onDragOver}
+                            nodes={state.editor.elements}
+                            // onNodesChange={onNodesChange}
+                            edges={edges}
+                            // onEdgesChange={onEdgesChange}
+                            // onConnect={onConnect}
+                            onInit={setReactFlowInstance}
+                            fitView
+                        // onClick={handleClickCanvas}
+                        // nodeTypes={nodeTypes}
+                        >
+
+                        </ReactFlow>
+
+
+                    </div>
+                </div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel
+                defaultSize={40}
+                className="relative sm:block"
+            >
+
+                <div>
+                    EditorcanvasSidebar here!!
+                </div>
+
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    )
+}
+
+export default EditorCanvas
