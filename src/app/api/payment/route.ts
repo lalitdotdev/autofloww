f9e127c98db9b9c3a77ebe77d760b72c81960994
Lalit sharma <geekymonkcp742k@gmail.com>
2024-04-17 09:13:23 +0530

    api: Implement Stripe checkout session creation (POST).

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -14,3 +14,23 @@ export async function GET(req: NextRequest) {
 
   return NextResponse.json(products.data);
 }
+
+export async function POST(req: NextRequest) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET!, {
+    typescript: true,
+    apiVersion: '2024-04-10',
+  });
+  const data = await req.json();
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: data.priceId,
+        quantity: 1,
+      },
+    ],
+    mode: 'subscription',
+    success_url: 'https://localhost:3000/billing?session_id={CHECKOUT_SESSION_ID}',
+    cancel_url: 'https://localhost:3000/billing',
+  });
+  return NextResponse.json(session.url);
+}
