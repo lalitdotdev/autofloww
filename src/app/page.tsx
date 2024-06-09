cc5bb8cb5310512ac7dc2f616ba32384d1d060df
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-09 16:33:17 +0530

    fix(home site page): layout fix and fixed overflow of root page layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import Navbar from '@/components/global/navbar'
 export default function Home() {
     //WIP: remove fault IMAge for home page
     return (
-        <main className="flex items-center justify-center flex-col mb-12">
+        <main className="flex items-center justify-center flex-col mb-12 overflow-hidden">
             <Navbar />
             <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
                 <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
